18f1b3a3aaf91741abcd4e5e8dca442330dcf708
Bùi Thị Phương Lâm <103896336+PhuongLamLucky@users.noreply.github.com>
2022-12-08 16:17:28 +0700

    lam

--- admin_login.php
+++ admin_login.php
@@ -8,7 +8,7 @@ session_start();
 include('db.php');
 
 ?>
-
+ 
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,21 +22,20 @@ include('db.php');
 
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css' integrity='sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==' crossorigin='anonymous'/>
 
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+  <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400,400i|Noto+Sans:400,400i,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet">
   <title>BULE SKY</title>
 </head>
 
 <div class="admin__login">
+<div class ="form">
     <form method="post">
-        <label>
-            Email
-        </label>
-        <input type="text" name="ad_email" placeholder="Nhập Email">
-        <label>
-            Mật khẩu
-        </label>
-        <input type="password" name="ad_pass" placeholder="Nhập mật khẩu ">
-        <button class="button" name="submit" type="submit">Đăng nhập</button>
+        <h2>ADMIN</h2>
+        <label style="margin-left: 0px;">Email</label>
+        <input style="width: 100%;" type="text" name="ad_email" placeholder="Nhập Email">
+        <label style="margin-left: 0px;"> Mật khẩu</label>
+        <input style="width: 100%; padding: 5px;" type="password" name="ad_pass" placeholder="Nhập mật khẩu ">
+        <button class="button1" style="margin-left: 20px;" type="submit" name="submit">Đăng nhập</button>
     </form>
 </div>
 </div>
@@ -44,7 +43,7 @@ include('db.php');
 if (isset($_POST['submit'])) {
     $ad_email = $_POST['ad_email'];
     $ad_pass = md5($_POST['ad_pass']);
-    $sql = "SELECT * FROM `admins` WHERE admin_email='$ad_email' AND admin_pass='$ad_pass'";
+    $sql = "SELECT * FROM `ad` WHERE ad_email='$ad_email' AND ad_pass='$ad_pass'";
     $res = mysqli_query($con, $sql);
     $count = mysqli_num_rows($res);
     // echo  $sql;
